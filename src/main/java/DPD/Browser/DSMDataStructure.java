c08299791f97df0168d0f41623080fb51d1bc93f
Justice Ogbonna <ebesco@yahoo.com>
2017-01-12 23:57:29 -0500

    moved depNode back to dsm ds

--- src/main/java/DPD/Browser/DSMDataStructure.java
+++ src/main/java/DPD/Browser/DSMDataStructure.java
@@ -1,6 +1,5 @@
 package DPD.Browser;
 
-import DPD.Model.DepNode;
 import DPD.Model.DependencyType;
 
 import java.util.ArrayList;
@@ -194,6 +193,20 @@ public class DSMDataStructure {
 
         public ClassNode(String path) {
             this.filePath = path;
+        }
+    }
+
+    class DepNode {
+        public int row; // classId on dependency row
+        public int col; // classId on dependent row
+        public String value;
+        public int numValue;
+
+        public DepNode(String data, int row, int col) {
+            this.value = data;
+            this.row = row;
+            this.col = col;
+            this.numValue = Integer.parseInt(this.value, 2);
         }
     }
 }
