4c20dd366bf57d3929f59537107b66ef2ce74413
Justice Ogbonna <ebesco@yahoo.com>
2017-03-23 17:08:53 -0400

    export dependencyType to scala, enhancements to dsmModel

--- src/main/java/DPD/Model/DSMModel_S.scala
+++ src/main/java/DPD/Model/DSMModel_S.scala
@@ -7,10 +7,18 @@ import scala.io.Source
   */
 class DSMModel_S {
 
-  def parse(path: String): (List[String], Int, List[String], List[String]) = {
+  /*
+    reads a dsm file and returns content as the tuple
+    (List[String] - dependency types used in dsm,
+      Int - number of files (or matrix size),
+      List[String] - the dsm matrix lines,
+      List[String] - the corresponding file paths of the matrix lines
+      )
+   */
+  def parse(path: String): (List[DependencyType_S.Value], Int, List[String], List[String]) = {
     val depLine::countLine::matrix_files = Source.fromFile(path).getLines().toList
     val count = Integer.parseInt(countLine)
-    (extractArray(depLine), count, matrix_files.take(count), matrix_files.takeRight(count))
+    (extractArray(depLine).map(DependencyType_S.withName), count, matrix_files.take(count), matrix_files.takeRight(count))
   }
 
   def extractArray(arg: String): List[String] =
---
+++ src/main/java/DPD/Model/DependencyType_S.scala
@@ -0,0 +1,22 @@
+package DPD.Model
+
+/**
+ * Created by Justice on 3/23/2017.
+ */
+object DependencyType_S extends Enumeration {
+    val
+    TYPED,
+    USE,
+    IMPLEMENT,
+    EXTEND,
+    AGGREGATE,
+    CALL,
+    SET,
+    IMPORT,
+    CREATE,
+    SPECIALIZE,
+    CAST,
+    THROW,
+    MODIFY
+        = Value
+}
