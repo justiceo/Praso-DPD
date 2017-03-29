45bac6c2a5fa6144eaa677d7264fb35f1821ff15
Justice Ogbonna <ebesco@yahoo.com>
2017-03-29 13:12:48 -0400

    added first test spec, and stuff works

---
+++ src/test/scala/DPD/DSMDataStructureSpec.scala
@@ -0,0 +1,9 @@
+package DPD
+
+import org.scalatest._
+
+class DSMDataStructureSpec extends FlatSpec with Matchers {
+    "The Hello Object" should "say hello" in {
+        "hello" shouldEqual "hello"
+    }
+}
