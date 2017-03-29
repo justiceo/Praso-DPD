ca61d1a856697416cabf7143c026a21bba03d5a4
Justice Ogbonna <ebesco@yahoo.com>
2017-03-28 22:17:02 -0400

    remove all java classes - we're going full-blown scala. Lord help me!

--- src/main/java/DPD/Model/BucketResult.java
+++
@@ -1,9 +0,0 @@
-package DPD.Model;
-
-/**
- * Created by I853985 on 1/13/2017.
- */
-public class BucketResult extends Bucket {
-    public Entity aux = new Entity();
-    public Entity pivot = new Entity();
-}
