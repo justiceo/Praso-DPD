898c310d4cb2f8cb3d725c8ce3d25e370cb0472a
Justice Ogbonna <ebesco@yahoo.com>
2017-04-17 13:01:45 -0400

    working on matrix, files not in order

--- src/main/scala/FuncDsm/FuncDsm.scala
+++ src/main/scala/FuncDsm/FuncDsm.scala
@@ -16,57 +16,71 @@ import DPD._
 
 object FuncDsm {
 
-    case class Csv(caller: String, line: Int, dependency: DependencyType.Value, callee: String)
+    case class Csv(function: String, file:String, line: Int, dependsOnFunction: String, dependsOnType: DependencyType.Value, dependsOnFile: String)
 
     def main(args: Array[String]): Unit = {
-        println("hello main works");
+        println("hello main works")
+        println(tokenize("org.apache.commons.compress.AbstractTestCase.addArchiveEntry(ArchiveOutputStream out,String filename,File infile),D:\\Code\\Tools\\art_tools\\scripts\\dump\\commons-compress\\src\\test\\java\\org\\apache\\commons\\compress\\AbstractTestCase.java,214,ArchiveOutputStream.createArchiveEntry,Call,D:\\Code\\Tools\\art_tools\\scripts\\dump\\commons-compress\\src\\main\\java\\org\\apache\\commons\\compress\\archivers\\ArchiveOutputStream.java"))
+        run("D:\\Code\\IdeaProjects\\DPD\\src\\main\\resources\\func-dependency.csv")
     }
 
     def run(file: String) = {
-        val lines = Source.fromFile(file).getLines();
-        val csv: List[Csv] = lines.map(l => tokenize(l)).toList; 
-        verifyCsv(csv); 
-        val dependencies: List[DependencyType.Value] = csv.map(_.dependency).distinct.sorted
-        val grouped = csv.map(l => (l.caller, dependencies.indexOf(l.dependency), csv.indexOf(l.callee))).groupBy(_._1)
+        val lines = Source.fromFile(file).getLines()
+        val csv: List[Csv] = lines.toList.tail.map(l => tokenize(l))
+        verifyCsv(csv)
+        val dependencies: List[DependencyType.Value] = csv.map(_.dependsOnType).distinct.sorted
+        val grouped = csv.map(l => (l.function, dependencies.indexOf(l.dependsOnType), getCsvIndex(csv, l.dependsOnFunction))).groupBy(_._1)
         val files = grouped.keys.toList
-        val matrix = grouped.map(t => serialize(t._2, dependencies.size))
+        val matrix = grouped.map(t => serialize(t._2, dependencies.size, files.size))
         val dsm = dependencies.mkString(",") + "\n" + files.size + "\n" + matrix.mkString("\n") + files.mkString("\n")
         println(dsm)
     }
 
-    def serialize(csv: List[(String, Int, Int)], depSize: Int): String = {
-        "dude";
+    def getCsvIndex(csvList: List[Csv], func: String): Int = {
+        val res = csvList.map(_.function).zipWithIndex.filter(t => t._1.contains("." + func))
+        if(res.isEmpty) 0
+        else res.head._2
+    }
+
+
+    def serialize(csv: List[(String, Int, Int)], depSize: Int, fileSize: Int): String = {
+        val depfuncs = csv.map(t => t._3);
+        (0 to fileSize).toList.map(_ => "0").zipWithIndex.map(t => {
+            if(depfuncs.contains(t._2)) {
+                "11111"
+            }
+            else t._1
+        }).mkString(" ")
     }
 
     def tokenize(line: String): Csv = {
-        val tokens = ListBuffer[String]();
+        val tokens = ListBuffer[String]()
         val iter = line.iterator
         var sb = new StringBuilder()
         while(iter.hasNext) {
             var ch = iter.next()
-            if(ch == ",") {
-                tokens += (sb.toString)
+            if(ch == ',') {
+                tokens += sb.toString
                 sb = new StringBuilder()
             }
-            else if(ch == "(") {
-                while(ch != ")") {
+            else if(ch == '(') {
+                while(ch != ')') {
                     sb.append(ch)
                     ch = iter.next()
                 }
                 sb.append(ch) // add closing ")"
             }
-            else sb.append(ch);
+            else sb.append(ch)
         }
-        return Csv(tokens(0), tokens(1).toInt, DependencyType.withName(tokens(2)), tokens(3));
+        tokens += sb.toString // empty string buffer
+        Csv(tokens(0), tokens(1), tokens(2).toInt, tokens(3), DependencyType.withName(tokens(4).toUpperCase()), tokens(5))
     }
 
     def verifyCsv(csvList: List[Csv]): Unit = {
         //all callees must end with ")"
         //all callers must end start with "D:Code",
         //the other two, Line and Dependency would throw an initialization exception otherwise
-        if (csvList.map(_.callee).exists(s => s.endsWith(")")) ) 
+        if (csvList.map(_.function).exists(s => !s.endsWith(")")) )
             throw new Exception("all callees must end with ')'")
-        if (csvList.map(_.callee).exists(s => !s.startsWith("D:/Code")) ) 
-            throw new Exception("all callers must begin with 'D:/Code'")
     }
 }
