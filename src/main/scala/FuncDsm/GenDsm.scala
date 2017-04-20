de067bf4a984e663b02b2d59686e7388c0fccfd6
Justice Ogbonna <ebesco@yahoo.com>
2017-04-20 11:53:48 -0400

    gendsm cleanup

--- src/main/scala/FuncDsm/GenDsm.scala
+++ src/main/scala/FuncDsm/GenDsm.scala
@@ -13,6 +13,7 @@ class GenDsm(csvList: List[FuncDsm.Csv]) {
     val dependsOnFunction = csvList.map(t => fqfunction(t.dependsOnFunction, t.dependsOnFile))
     lazy val functions = (csvList.map(_.function) ::: dependsOnFunction).map(noargs).distinct.sorted 
     lazy val zeroDepFunc: List[String] = (0 until functions.size).toList.map(_ => "0")
+    
     // group all the csvList by function
     val thinned: List[(String, String, String)] = csvList.map(t => (noargs(t.function), depString(t.dependsOnType), fqfunction(t.dependsOnFunction, t.dependsOnFile)))
     val grouped: Map[String, List[(String, String, String)]] = thinned.groupBy(_._1)
@@ -50,6 +51,7 @@ class GenDsm(csvList: List[FuncDsm.Csv]) {
         rep(index)= '1'
         rep.mkString
     }
+
     // given a package prefix, function and it's file
     // returns the combined universal identifier for the function
     def fqfunction(function:String, file:String):String = {
@@ -61,18 +63,6 @@ class GenDsm(csvList: List[FuncDsm.Csv]) {
             return trimmedFile + "." + dotRest(function)
         else if(trimmedFile.endsWith(function)) return trimmedFile
         else return trimmedFile + "." + function
-    }
-
-    // given the number of total number of functions, the size of dependency string,
-    // generate a dependency binary string "0010" for a function againt the other functions
-    def serialize(csv: List[(String, Int, String)], depSize: Int, fileSize: Int): String = {
-        val depfuncs = csv.map(t => t._3)
-        (0 to fileSize).toList.map(_ => "0").zipWithIndex.map(t => {
-            if(depfuncs.contains(t._2)) {
-                "11111"
-            }
-            else t._1
-        }).mkString(" ")
     }
 
     def printStr:String = dependencies.mkString(",") + "\n" + functions.size + "\n" + matrix.mkString("\n") + "\n" + functions.mkString("\n")
