ec23a7a7fdb61705d497f3156c214cc7b05de3de
Justice Ogbonna <ebesco@yahoo.com>
2017-05-09 18:35:32 -0400

    add java if not exists

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# If Java is not installed, install it
+if [ -n `which java` ]; then
+  sudo apt-get install openjdk-8-jre
+fi
+
 
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
