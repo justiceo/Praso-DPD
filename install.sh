9412c92992e1cc550086ad5c787c4aa19c9e5678
Justice Ogbonna <ebesco@yahoo.com>
2017-05-09 18:28:54 -0400

    ensure script is run as root user

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Ensure we're running as root user
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt-get update
